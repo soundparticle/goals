82687bd28ff85f13624976e832c1acc8d7c3d176
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-06 13:06:46 -0700

    console logs added for troubleshooting

--- src/store/promise-middleware.js
+++ src/store/promise-middleware.js
@@ -5,8 +5,9 @@ const isPromise =  val => val && typeof val.then === 'function';
 export default ({ dispatch }) => next => action => {
 
   const { type, payload } = action;
+  console.log('*** action', action);
+  console.log('*** type', type);
   if(!isPromise(payload)) return next(action);
-
   dispatch({ type: LOADING_START });
 
   return payload
