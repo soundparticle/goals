8b1157761adb7a8982f30ed658228d67f65789cd
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-11 16:35:08 -0700

    fixed signin path in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ const GOALS_URL = `${URL}/goals`;
 const AUTH_URL = `${URL}/auth`;
 const SIGNUP_URL = `${AUTH_URL}/signup`;
 const SIGNIN_URL = `${AUTH_URL}/signin`;
-export const signin = credentials => post(`${SIGNUP_URL}/signin`, credentials);
-export const signup = credentials => post(`${SIGNIN_URL}/signup`, credentials);
+export const signin = credentials => post(`${SIGNIN_URL}/signin`, credentials);
+export const signup = credentials => post(`${SIGNUP_URL}/signup`, credentials);
 
 export const verifyUser = token => get(`${AUTH_URL}/verify`, {
   headers: {
@@ -16,7 +16,6 @@ export const verifyUser = token => get(`${AUTH_URL}/verify`, {
 
 export const getGoals = () => get(`${GOALS_URL}`);
 export const postGoal = data => post(`${GOALS_URL}`, data);
-// export const toggleGoal = id  => post(`${GOALS_URL}/${id}`);
 export const changeGoalStatus = id => post(`${GOALS_URL}/${id}`);
 export const getUsersGoals = () => get(`${URL}/users`);
 
